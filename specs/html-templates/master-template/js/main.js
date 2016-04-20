bdfd58540256d2741f773de36319264b3fe06ea9
blueflymedia <paul.blueflymedia@gmail.com>
2016-04-20 09:13:57 -0400

    update

--- specs/html-templates/master-template/js/main.js
+++ specs/html-templates/master-template/js/main.js
@@ -73,9 +73,16 @@ $('.product-slider').slick({
         breakpoint: 1024,
         settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         infinite: true,
         dots: false
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
         }
     },
     {
